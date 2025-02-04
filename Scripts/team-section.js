cd5a8d25da08647912df95d73ba540ed6b162181
Anushka Dubey <136051483+anushkaa-dubey@users.noreply.github.com>
2025-02-04 20:29:00 +0530

    event section

--- Scripts/team-section.js
+++ Scripts/team-section.js
@@ -190,3 +190,13 @@ navItemsFirst.forEach(item => {
 });
 
 
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  setTimeout(() => {
+    preloader.style.opacity = "0"; // Fade out effect
+    setTimeout(() => {
+      preloader.style.display = "none"; // Remove preloader from view
+      document.querySelector(".content").style.display = "block"; // Show content
+    }, 500); // Duration of fade-out effect
+  }, 3900); // Delay the loader for 3 seconds (adjust as needed)
+});
